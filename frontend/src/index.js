2e54638d36e11c412c46b5180e52282e68a5b4db
Prabin Gautam <prabeen122@gmail.com>
2022-01-21 18:53:19 +0545

    Simple Loading Added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Login from './forms/Login';
 import './forms/Table.css';
+import Loading from './components/common/Loading';
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path={'/login'} element={<Login />} />
+      <Route path='/loading' element={<Loading />} />
       <Route path={'/*'} element={<App />} />
     </Routes>
   </BrowserRouter>,
