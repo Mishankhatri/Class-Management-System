db406cf9260824ff51862c63faff7237a9977a1c
Prabin Gautam <prabeen122@gmail.com>
2022-01-31 12:48:37 +0545

    Settings Added

---
+++ frontend/src/components/adminpanel/pages/users/CustomSettings.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { HeaderInputField } from './../../common/InputField/HeaderInputField';
+
+function CustomSettingsInput({
+  title,
+  icon,
+  required,
+  type,
+  placeholder,
+  name,
+  register,
+  value,
+}) {
+  return (
+    <React.Fragment>
+      <div className='inputfield'>
+        <HeaderInputField
+          title={title}
+          icon={icon}
+          isRequired={required}
+          className='headerinputfield'
+        />
+        <div className=''>
+          <input
+            type={type}
+            className='input'
+            placeholder={placeholder}
+            {...register(name)}
+            required
+            name={name}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default CustomSettingsInput;
