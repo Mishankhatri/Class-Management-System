91fb3d4e058e49588756f56f75b160a35562eb18
Mishan khatri <mishankhatri490@gmail.com>
2022-02-16 18:53:20 +0545

    fixed login redirect on page refresh and changed subject_code to charfield

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import "./components/adminpanel/pages/student/CustomView.css";
 
 import "./components/common/css/Announcement.css";
 import "./components/common/css/CustomConfirm.css";
+import { getUser } from "./redux/actions/authactions";
 //Alert options
 const alertOptions = {
   timeout: 2000,
@@ -25,6 +26,10 @@ const alertOptions = {
     zIndex: 99999,
   },
 };
+const token = localStorage.getItem("access_token");
+if (token) {
+  store.dispatch(getUser());
+}
 
 ReactDOM.render(
   <BrowserRouter>
