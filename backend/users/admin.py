08247be246e7232c6631b685e429530e4a84623e
Mishan khatri <mishankhatri490@gmail.com>
2022-02-20 22:43:35 +0545

    deleted fullname from admin_user_model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -10,13 +10,13 @@ CMS_Users = get_user_model()
 
 class UserAdminConfig(UserAdmin):
     model = CMS_Users
-    search_fields = ('email', 'username', 'fullname',)
+    search_fields = ('email', 'username',)
     list_filter = ('admin', 'teacher','student',)
     ordering = ('-account_created',)
-    list_display = ('email','id', 'username', 'fullname',
+    list_display = ('email','id', 'username',
                     'is_active', 'is_staff','admin','teacher','student',)
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'fullname','password','profile_image')}),
+        (None, {'fields': ('email', 'username','password','profile_image')}),
         ('Permissions', {'fields': ('is_staff','is_active', 'admin','teacher','student','groups', 'user_permissions')}),
     )
     formfield_overrides = {
@@ -25,7 +25,7 @@ class UserAdminConfig(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'fullname', 'password1', 'password2', 'is_active','admin', 'teacher','student')}
+            'fields': ('email', 'username','password1', 'password2', 'is_active','admin', 'teacher','student')}
          ),
     )
 
